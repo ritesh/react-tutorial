97d827afe594d2f5c884f7e53bcd667e7def0ceb
Ritesh Sinha <riteshsinha@Riteshs-MacBook-Pro.local>
2017-01-13 22:11:42 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './App';
+import './index.css';
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('container')
+);
